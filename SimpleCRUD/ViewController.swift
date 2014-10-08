687d9b8fb6bf6bd795f728301a20d6019c671713
Saravanakumar TIruthani <TVS@Saravanakumars-MacBook-Pro.local>
2014-10-08 10:46:37 -0700

    Initial Commit

---
+++ SimpleCRUD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCRUD
+//
+//  Created by Saravanakumar TIruthani on 10/8/14.
+//  Copyright (c) 2014 Kukumber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
